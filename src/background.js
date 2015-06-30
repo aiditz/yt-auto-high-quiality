4b0c37a99589e8bb61de830bb5fbe453abf9f161
aiditz <divineone@yandex.ru>
2015-07-01 00:29:40 +0300

    aiditz blocker initial commit

---
+++ src/background.js
@@ -0,0 +1,66 @@
+
+var blockList = [
+	{
+		page : 'vk\\.com\\/feed$',
+		class: 'post',
+		attr : 'data-ad-view'
+	}, 
+	{
+		page : 'vk\\.com\\/feed$',
+		class: 'ads_ads_news_wrap'
+	}, 
+	{
+		page: 'vk\\.com\\/feed\\?section=recommended',
+		id  : 'feed_recommends'
+	}
+];
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  
+	if (changeInfo.status != 'complete') return;
+		
+	var code = '';
+  
+	for (var k in blockList) {
+		var item = blockList[k];
+		if (new RegExp(item.page).test(tab.url.toLowerCase())) {
+			
+			if (item.id) {
+				code += "var el = document.getElementById('" + item.id + "');" + 
+				        "if (el) el.parentNode.removeChild(el);";
+			}
+			else if (item.class) {
+				code += "var els = document.getElementsByClassName('" + item.class + "');" + 
+				        "for(var k = 0; k < els.length; k++) {" +	
+				        "  var el = els[k];";
+			
+				if (item.attr) {
+					code += "  if (el.getAttribute('" + item.attr + "'))";
+				}
+				
+				code += "  el.parentNode.removeChild(el);" +
+				        "}";
+			}
+			else if (item.tag) {
+				code += "var els = document.getElementsByTagName('" + item.tag.toUpperCase() + "');" + 
+				        "for(var k = 0; k < els.length; k++) {" +	
+				        "  var el = els[k];";
+			
+				if (item.attr) {
+					code += "  if (el.getAttribute('" + item.attr + "'))";
+				}
+				
+				code += "  el.parentNode.removeChild(el);" +
+				        "}";
+			}
+		};
+	}
+  
+	if (code == '') {
+		return;
+	}
+	
+	setTimeout(function() {
+		chrome.tabs.executeScript(tab.id, {code: code});
+	}, 0);
+});
